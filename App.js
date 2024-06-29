bb8338d1bdcac7d520263dbd16285b946c7bb164
elsafir <alfulsafiraaa08@gmail.com>
2024-06-30 05:37:59 +0700

    Initial commit

--- App.js
+++ App.js
@@ -1,20 +1,66 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+// Import axios, BASE_URL, dan API_KEY
+import axios from 'axios';
+import { BASE_URL, API_KEY } from './src/constant';
+import { View, StyleSheet, ScrollView } from 'react-native';
+import WeatherSearch from './src/components/weatherSearch';
+import WeatherInfo from './src/components/weatherInfo';
 
-export default function App() {
+const App = () => {
+  // Definisikan state "weatherData" dan "setWeatherData"
+  const [weatherData, setWeatherData] = useState();
+  const searchWeather = (location) => {
+    axios
+      .get(`${BASE_URL}?q=${location}&appid=${API_KEY}`)
+      .then((response) => {
+        const data = response.data;
+        // Tambahkan code di bawah
+        data.visibility /= 1000;
+        data.visibility = data.visibility.toFixed(2);
+        data.main.temp -= 273.15; // Konversi Kelvin ke Celcius
+        data.main.temp = data.main.temp.toFixed(2);
+        setWeatherData(data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <ScrollView contentContainerStyle={styles.scrollContainer}>
+        <WeatherSearch searchWeather={searchWeather} />
+        {/* Tampilkan data cuaca ketika ada weatherData */}
+        {weatherData && <WeatherInfo weatherData={weatherData} />}
+      </ScrollView>
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#f0f4f8',
+  },
+  scrollContainer: {
+    padding: 20,
     alignItems: 'center',
     justifyContent: 'center',
   },
+  searchBox: {
+    marginVertical: 20,
+    width: '100%',
+  },
+  weatherInfoBox: {
+    marginTop: 20,
+    width: '100%',
+  },
+  header: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 20,
+    textAlign: 'center',
+  },
 });
+
+export default App;
---
+++ src/constant.js
@@ -0,0 +1,2 @@
+export const BASE_URL = 'https://api.openweathermap.org/data/2.5/weather';
+export const API_KEY = 'ab7e21de9ced69334297ee0da20955af';
